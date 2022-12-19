ca6a2de8e542305a6c7374a137ed5a87d2176185
BallinGuy <tballin2000@hotmail.com>
2022-12-19 10:40:30 -0500

    added project

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,6 +5,7 @@ import Jate from '../../assets/JATE.png';
 import Wip from '../../assets/work-in-progress.jpg';
 import Kwip from '../../assets/keyboard-work-in-progress.jpg';
 import Soon from '../../assets/coming-soon.jpg';
+import Technigram from '../../assets/Technigram.jpg';
 
 import { ImNewTab } from 'react-icons/im';
 import { FaGithubSquare } from 'react-icons/fa';
@@ -42,16 +43,16 @@ export default function Portfolio() {
         </div>
       </figure>
       <figure className='d-flex flex-column align-items-center figure'>
-        <img src={Wip} className='images wipImage'></img>
+        <img src={Technigram} className='images'></img>
         <div className='figLinkContainer'>
         <IconContext.Provider value={{size: '3em'}}>
-          <a href='https://github.com/ThatBallinGuy/' className='figLink' target="_blank"><FaGithubSquare /></a>
-          <a  className='figLink' target="_blank"><ImNewTab /></a>
+          <a href='https://github.com/PeteChicchetti/technigram' className='figLink' target="_blank"><FaGithubSquare /></a>
+          <a href='https://murmuring-depths-50021.herokuapp.com/builds/639e76bf0d78e50fc8cd5a0a' className='figLink' target="_blank"><ImNewTab /></a>
           </IconContext.Provider>
         </div>
       </figure>
       <figure className='d-flex flex-column align-items-center figure'>
-        <img src={Kwip} className='images wipImage'></img>
+        <img src={Wip} className='images wipImage'></img>
         <div className='figLinkContainer'>
         <IconContext.Provider value={{size: '3em'}}>
           <a href='https://github.com/ThatBallinGuy/' className='figLink' target="_blank"><FaGithubSquare /></a>
